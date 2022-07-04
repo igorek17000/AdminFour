5e0833af8c40085e8dbeef76b7ab9f8d9e064bfc
EC2 Default User <ec2-user@ip-172-31-90-223.ec2.internal>
2022-07-04 02:27:02 +0000

    Pre-Apache Push

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,6 @@ module AdminFour
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+#config.hosts = "vedoc-admin-load-balancer-631166647.us-east-1.elb.amazonaws.com"
   end
 end
