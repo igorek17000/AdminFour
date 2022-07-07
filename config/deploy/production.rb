7fc1f20ef699392478bd579305423f49932ea650
TheTactic <cheesetoochalk@yahoo.com>
2022-07-07 11:21:41 -0500

    Update production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,3 @@
 #FIXME replace 1.2.3.4 with your IP address
-server '3.91.214.79', user: 'deploy', roles: %w{web app db}
+server '54.175.244.248', user: 'deploy', roles: %w{web app db}
 set :rails_env, 'production'
